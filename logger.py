5b78940f3ec0717754ff1b49408740e8c89e09bc
carlzoo <carl.j.zhou@protonmail.com>
2019-11-20 20:39:30 -0500

    add logger

---
+++ logger.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+logger = logging.getLogger('scalperr_api')
+logger.setLevel(logging.DEBUG)
+console_log_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - [%(levelname)s] %(name)s - %(message)s')
+console_log_handler.setFormatter(formatter)
+console_log_handler.setLevel(logging.DEBUG)
+logger.addHandler(console_log_handler)
