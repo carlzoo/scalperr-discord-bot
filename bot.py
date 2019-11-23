9bbf8d5db7e2465c876950535f5ddc6728488745
carlzoo <carl.j.zhou@protonmail.com>
2019-11-23 14:16:16 -0500

    refactor code in main python file

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ import os
 from logger import logger
 from httpclient import http_get
 
-
-from discord import Embed, Colour
+from discord import Colour
 from discord.ext import commands
 from dotenv import load_dotenv
+
+from bot_response import BotResponse
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -32,25 +33,18 @@ async def _tickemaster_count(ctx, *, event_id):
     url = f'https://yrmfkazv8g.execute-api.ca-central-1.amazonaws.com/dev_ca_central_1/' \
           f'tmstockchecker?access_code={api_access_code}&event_id={event_id}'
     json_response = await http_get(url)
-    if not json_response:
+    if not json_response or "sections" not in json_response:
         await ctx.send("Error fetching info from Ticketmaster")
         return
 
     # create the embed
-    em = Embed(
-        title='Ticketmaster Inventory Count',
-        colour=Colour.red()
-    )
-    if not json_response["sections"] or len(json_response["sections"]) == 0:
-        em.add_field(name=f'Error', value=f'No seats available', inline=False)
-    for section in json_response["sections"]:
-        display_min_price = f'${section["min_price"]}'
-        display_max_price = f'${section["max_price"]}'
-        display_price = f'{display_min_price} - {display_max_price}'
-        em.add_field(name=f'Section {section["section"]}', value=f'{display_price} Count: {section["count"]}',
-                     inline=False)
-    em.set_footer(text="Stock Checker made by ieatgoosefeces#2163")
-    await ctx.send(embed=em)
+    bot_msg = BotResponse("Ticketmaster Inventory Count", Colour.red())
+    if len(json_response["sections"]) == 0:
+        bot_msg.set_empty()
+    else:
+        for section in json_response["sections"]:
+            bot_msg.add_section(section["section"], section["min_price"], section["max_price"],section["count"])
+    await ctx.send(embed=bot_msg)
 
 
 @client.command(aliases=['tmintl'])
@@ -63,20 +57,12 @@ async def _tickemaster_intl_count(ctx, *, event_id):
         return
 
     # create the embed
-    em = Embed(
-        title='Ticketmaster Inventory Count',
-        colour=Colour.red()
-    )
+    bot_msg = BotResponse("Ticketmaster International Inventory Count", Colour.blue())
     if not json_response["sections"] or len(json_response["sections"]) == 0:
-        em.add_field(name=f'Error', value=f'No seats available', inline=False)
+        bot_msg.set_empty()
     for section in json_response["sections"]:
-        display_min_price = f'${section["min_price"]}'
-        display_max_price = f'${section["max_price"]}'
-        display_price = f'{display_min_price} - {display_max_price}'
-        em.add_field(name=f'Section {section["section"]}', value=f'{display_price} Count: {section["count"]}',
-                     inline=False)
-    em.set_footer(text="Stock Checker made by ieatgoosefeces#2163")
-    await ctx.send(embed=em)
+        bot_msg.add_section(section["section"], section["min_price"], section["max_price"], section["count"])
+    await ctx.send(embed=bot_msg)
 
 
 @client.command(aliases=['axs'])
@@ -89,20 +75,12 @@ async def _axs_count(ctx, *, event_id):
         return
 
     # create the embed
-    em = Embed(
-        title='AXS Inventory Count',
-        colour=Colour.red()
-    )
+    bot_msg = BotResponse("AXS Inventory Count", Colour.dark_blue())
     if not json_response["sections"] or len(json_response["sections"]) == 0:
-        em.add_field(name=f'Error', value=f'No seats available', inline=False)
+        bot_msg.set_empty()
     for section in json_response["sections"]:
-        display_min_price = f'${section["min_price"]}'
-        display_max_price = f'${section["max_price"]}'
-        display_price = f'{display_min_price} - {display_max_price}'
-        em.add_field(name=f'Section {section["section"]}', value=f'{display_price} Count: {section["count"]}',
-                     inline=False)
-    em.set_footer(text="Stock Checker made by ieatgoosefeces#2163")
-    await ctx.send(embed=em)
+        bot_msg.add_section(section["section"], section["min_price"], section["max_price"], section["count"])
+    await ctx.send(embed=bot_msg)
 
 
 if __name__ == "__main__":
