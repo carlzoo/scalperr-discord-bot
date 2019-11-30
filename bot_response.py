2e66dea69b65e54da9e961f401c89f733aad0390
carlzoo <carl.j.zhou@protonmail.com>
2019-11-29 19:51:55 -0500

    add more properties to bot response, add help command message

--- bot_response.py
+++ bot_response.py
@@ -5,19 +5,46 @@ class BotResponse(Embed):
     def __init__(self, title, event_id, colour):
         super().__init__(title=title, colour=colour)
         self.event_id = event_id
-        self.title_msg = title
+        self.title = title
+        self.event_name = ''
+        self.city = ''
+        self.venue = ''
+        self.url = ''
+        self.datetime = ''
         self.em_colour = colour
         self.footer_msg = "Stock Checker made by ieatgoosefeces#2163"
         self.sections = []  # tuple of (section, count)
         self.total = 0
 
+    def set_datetime(self, datetime):
+        self.datetime = datetime
+
+    def set_url(self, url):
+        self.url = url
+
+    def set_venue(self, venue):
+        self.venue = venue
+
+    def set_name(self, event_name):
+        self.event_name = event_name
+
+    def set_city(self, city):
+        self.city = city
+
     def add_section(self, section_name, count):
         self.sections.append((section_name, count))
         self.total += count
 
-    def build_display(self):
-        self.add_field(name=f'Event Link',
-                       value=f'[{self.event_id}](https://www1.ticketmaster.com/events/{self.event_id})',
+    def build_display(self, is_help=False):
+        if is_help:
+            self.add_field(name=f'Commands',
+                           value=f'!stock tm [event_id]\n!stock tmintl [event_id]\n!stock axs [event_id]',
+                           inline=False)
+            self.set_footer(text="Stock checker made by ieatgoosefeces#2163")
+            return
+
+        self.add_field(name=f'Event Info',
+                       value=f'[{self.event_name}]({self.url})\n{self.venue} | {self.city}\n{self.datetime}',
                        inline=False)
         if self.total == 0:
             self.add_field(name=f'Error', value=f'No seats available', inline=False)
